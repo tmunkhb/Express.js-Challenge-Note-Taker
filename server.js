12931f8a8910ce4785f887108bebf225143f3080
tmunkhb <tmunkhb@gmail.com>
2022-10-19 21:07:43 -0400

    api/html routes

--- server.js
+++ server.js
@@ -15,21 +15,38 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // function for creating new notes
-GenerateNewNote = (body, notesArray) => {
-    const note = body;
-    notesArray.push(note);
+GenerateNewNote = (body, noteArray) => {
+    const newNote = body;
+    noteArray.push(newNote);
     fs.writeFileSync(
-        path.join(__dirname, '../db/db.json'),
-        JSON.stringify({note}, null, 2)
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes: noteArray}, null, 2)
     );
-    return note;
-}
+    return newNote;
+};
 
 
 
+// API routes
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = GenerateNewNote(req.body, notes);
+    res.json(note);
+})
 
 
 
+// html routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
